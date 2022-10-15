2dcec01375c1a47b4f288001ee3159e5825dfabd
Arj2001 <75932871+Arj2001@users.noreply.github.com>
2022-10-15 09:29:42 +0530

    one way and two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewRailwayComponent } from './view-railway/view-railway.component';
+
+const addRoute:Routes = [
+  {
+    path:"",component:ViewRailwayComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewRailwayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(addRoute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/view-railway/view-railway.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-railway',
+  templateUrl: './view-railway.component.html',
+  styleUrls: ['./view-railway.component.css']
+})
+export class ViewRailwayComponent implements OnInit {
+
+  constructor() { }
+
+  name="Arjun"
+
+  ngOnInit(): void {
+  }
+
+}
